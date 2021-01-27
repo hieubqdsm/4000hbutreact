4d642f7a977da34fb28f466d1cf2757aa9aade2e
Hieu Bui <hieu.bui@anphabe.com>
2021-01-27 11:50:24 +0700

    4000h

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const deathLine = "Friday, 4 June 2021, 18:50:00";
+    const calculateTimeLeft = () => {
+        let year = new Date().getFullYear();
+        const difference = +new Date(deathLine) - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    const [year] = useState(new Date().getFullYear());
+    useEffect(() => {
+        setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+    });
+
+    const timerComponents = [];
+
+    Object.keys(timeLeft).forEach((interval) => {
+        if (!timeLeft[interval]) {
+            return;
+        }
+
+        timerComponents.push(
+            <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+        );
+    });
+
+
+    const calculateHoursLeft = () => {
+        const different = +new Date(deathLine) - +new Date();
+        let hoursLeft = {};
+        if (different > 0) {
+            hoursLeft = {
+                hours: Math.floor((different / (1000 * 60 * 60))),
+            }
+        }
+        return hoursLeft;
+    }
+    const hourComponents = [];
+    const [hoursLeft, setHoursLeft] = useState(calculateHoursLeft());
+    Object.keys(hoursLeft).forEach((interval) => {
+        if (!hoursLeft[interval]) {
+            return;
+        }
+
+        hourComponents.push(
+            <span>
+        {hoursLeft[interval]}{"h left!"}
+      </span>
+        );
+    });
+
+    useEffect(() => {
+        setTimeout(() => {
+            setHoursLeft(calculateHoursLeft());
+        }, 1000);
+    })
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {hourComponents.length ? hourComponents : <span>Time's up!</span>}
+                </p>
+                <p>
+                    {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+                </p>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
